a626c81010b57386fa3891fc9702b1569ecbb3e6
masterUNG <phrombutr@gmail.com>
2016-03-01 16:46:17 +0700

    Finish 1March16

--- app/src/main/java/appewtc/masterung/banja2restaurant/ReadOrderListView.java
+++ app/src/main/java/appewtc/masterung/banja2restaurant/ReadOrderListView.java
@@ -3,10 +3,21 @@ package appewtc.masterung.banja2restaurant;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class ReadOrderListView extends AppCompatActivity {
 
@@ -33,6 +44,40 @@ public class ReadOrderListView extends AppCompatActivity {
 
     }   // Main Method
 
+    public void clickOrder(View view) {
+
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        for (int i=0;i<foodStrings.length;i++) {
+
+            try {
+
+                ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+                nameValuePairs.add(new BasicNameValuePair(MyManage.column_officer, officerString));
+                nameValuePairs.add(new BasicNameValuePair(MyManage.column_desk, deskString));
+                nameValuePairs.add(new BasicNameValuePair(MyManage.column_food, foodStrings[i]));
+                nameValuePairs.add(new BasicNameValuePair(MyManage.column_amount, amountStrings[i]));
+
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/29feb/php_add_order.php");
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+                httpClient.execute(httpPost);
+
+                Toast.makeText(ReadOrderListView.this, "Update Finish", Toast.LENGTH_SHORT).show();
+
+
+
+            } catch (Exception e) {
+                Toast.makeText(ReadOrderListView.this, "Cannot Update", Toast.LENGTH_SHORT).show();
+            }
+
+        }   // for
+
+    }   // clickOrder
+
     public void clickMore(View view) {
         finish();
     }
@@ -44,8 +89,8 @@ public class ReadOrderListView extends AppCompatActivity {
         Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM " + MyManage.order_table, null);
         cursor.moveToFirst();
 
-        String[] foodStrings = new String[cursor.getCount()];
-        String[] amountStrings = new String[cursor.getCount()];
+         foodStrings = new String[cursor.getCount()];
+         amountStrings = new String[cursor.getCount()];
 
         for (int i=0;i<cursor.getCount();i++) {
             foodStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_food));
